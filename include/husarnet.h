1241bddc0c3728e8cd3dce56958b13f946110a17
Milosz Lagan <milosz.lagan@husarion.com>
2024-08-05 11:58:36 +0200

    Release v2.0.0-beta4

--- include/husarnet.h
+++ include/husarnet.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#if defined(ARDUINO)
+  #if !defined(ARDUINO_ARCH_ESP32)
+    #error "This library only supports boards from the ESP32 family."
+  #elif ESP_ARDUINO_VERSION < ESP_ARDUINO_VERSION_VAL(3, 0, 3)
+    #error "This library requires ESP32 Arduino Core version 3.0.3 or newer. Please upgrade your board/platform."
+  #endif
+#endif
+
 // User-facing API is defined in the
 // "husarnet/port/esp32/user_interface.h" header
 // in the included Husarnet submodule.
 #include "husarnet/ports/esp32/user_interface.h"
-
-#if defined(ARDUINO) && !defined(ARDUINO_ARCH_ESP32)
-  #error "This library only supports boards from the ESP32 family."
-#endif
